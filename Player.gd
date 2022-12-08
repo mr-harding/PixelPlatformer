f6c1f9c5e39ecafb6d7bc5a66a602d22b444bab8
mr-harding <45374233+mr-harding@users.noreply.github.com>
2022-12-08 15:09:39 +1300

    video 3 complete
    Playing with animations, adding a new skin.
    I modified from tutorial, requiring user to press down to activate extra gravity and changing skin

--- Player.gd
+++ Player.gd
@@ -7,9 +7,6 @@ export(int) var MAX_SPEED = 50
 export(int) var ACCELERATION = 10
 export(int) var FRICTION = 10
 export(int) var GRAVITY = 4
-export(int) var ADDITIONAL_FALL_GRAVITY = 8
-
-
 
 func _ready():
 	pass
@@ -19,23 +16,31 @@ func _physics_process(delta):
 	var input = Vector2.ZERO
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
+	
 	if input.x == 0:
 		apply_friction()
+		$AnimatedSprite.animation = "idle"
 	else:
 		apply_acceleration(input.x)
-
+		$AnimatedSprite.animation = "Run"
+		if input.x > 0:
+			$AnimatedSprite.flip_h = true
+		elif input.x < 0:
+			$AnimatedSprite.flip_h = false
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	if is_on_floor():
-
 		if Input.is_action_pressed("ui_up"):
 			velocity.y = JUMP_FORCE
+			
 	else:
+		$AnimatedSprite.animation = "jump_slow"
 		if Input.is_action_just_released("ui_up") and velocity.y < -70:
 			velocity.y = RELEASE_FORCE
 			
-		if velocity.y > 0:
-			velocity.y += ADDITIONAL_FALL_GRAVITY
+		if Input.is_action_pressed("ui_down"):
+			velocity.y += 30
+			$AnimatedSprite.animation = "jump"
 		
 func apply_gravity():
 	velocity.y += GRAVITY
